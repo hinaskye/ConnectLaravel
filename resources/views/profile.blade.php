d23f510a10689b194be9c73961bb7299c3f423ce
rhysellwood <s3491191@student.rmit.edu.au>
2017-08-25 14:15:38 +1000

    Editing database to have an about me page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,34 +2,6 @@
 @extends('layouts.app')
 
 
-<!-- // $servername = "connectdb.ckktlmrdu53g.ap-southeast-2.rds.amazonaws.com";
-// $username = "ConnectAdmin";
-// $password = "password";
-// $dbname = "UserDB";
-//
-// // Create Connnection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check Connnection
-// if ($conn->connect_error)
-//   {
-//     die("Connection failed: " . $conn->connect_error);
-//   } -->
-
-<!-- // $sql = "SELECT firstname, lastname FROM UserDB";
-// $result = $conn->query($sql);
-//
-// if ($result->num_rows > 0) {
-//     // output data of each row
-//     while($row = $result->fetch_assoc()) {
-//         // echo "- Name:". $row["firstname"];
-//     }
-// } else {
-//     echo "0 results";
-// }
-//
-// $conn->close(); -->
-
-
 <?php
   use Illuminate\Support\Facades\Auth;
   $user = Auth::user();

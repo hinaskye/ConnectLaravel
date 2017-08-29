415fc5c0a6850bb847748fba38005ea061f4defd
Michael <s3539352@student.rmit.edu.au>
2017-08-29 15:28:58 +1000

    updated profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,38 +2,9 @@
 @extends('layouts.app')
 
 
-<!-- // $servername = "connectdb.ckktlmrdu53g.ap-southeast-2.rds.amazonaws.com";
-// $username = "ConnectAdmin";
-// $password = "password";
-// $dbname = "UserDB";
-//
-// // Create Connnection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check Connnection
-// if ($conn->connect_error)
-//   {
-//     die("Connection failed: " . $conn->connect_error);
-//   } -->
-
-<!-- // $sql = "SELECT firstname, lastname FROM UserDB";
-// $result = $conn->query($sql);
-//
-// if ($result->num_rows > 0) {
-//     // output data of each row
-//     while($row = $result->fetch_assoc()) {
-//         // echo "- Name:". $row["firstname"];
-//     }
-// } else {
-//     echo "0 results";
-// }
-//
-// $conn->close(); -->
-
-
 <?php
-  use Illuminate\Support\Facades\Auth;
+
   $user = Auth::user();
-echo $user->username;
 ?>
 
 @section('content')
@@ -43,12 +14,12 @@ echo $user->username;
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">USer Details:</div>
+                <div class="panel-heading">User Details:</div>
 
                 <div class="panel-body">
 
                   <div class="card col-md-4 col-sm-6">
-                      <p class="matchingPercent">99%</p>
+                      <p class="matchingPercent">Match Percent: 99%</p>
                       <img class="card-img-top" src="https://i.ytimg.com/vi/2qZHh_iN5Zs/hqdefault.jpg" width="100%" alt="Match Image">
                       <div class="card-body">
                           <h3 class="card-title"><?php echo $user->username; ?></h3>
@@ -68,3 +39,12 @@ echo $user->username;
 
 </div>
 @endsection
+
+
+
+
+
+  <!-- $user = Auth::user();
+echo $user->username;
+
+ echo $user->username;  -->

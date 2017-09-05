7f18ee9400014c401719f094bbbdc6a68190e6d9
Hieu Ngo <s3502260@student.rmit.edu.au>
2017-09-05 14:30:29 +1000

    Added js to change bg gradient of match percentage

---
+++ public/js/home.js
@@ -0,0 +1,22 @@
+/* Run script straight away */
+var matchPercent = document.getElementsByClassName("matchingPercent");
+for(var i=0; i<matchPercent.length; i++) {
+    var percent_value = matchPercent[i].innerHTML.replace(/%/g,'');
+    if(percent_value<= 60) {
+        w3.addClass(matchPercent[i],'bg-red-xs');
+    }
+    else if(percent_value <= 70) {
+        w3.addClass(matchPercent[i], 'bg-red-s')
+    }
+    else if(percent_value <= 80) {
+        w3.addClass(matchPercent[i], 'bg-red-m')
+    }
+    else if(percent_value <= 90) {
+        w3.addClass(matchPercent[i], 'bg-red-l')
+    }
+    else if(percent_value <= 100) {
+        w3.addClass(matchPercent[i], 'bg-red-xl')
+    }
+}
+
+/* Functions */

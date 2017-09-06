fc247fe05c21000af49c56046b4046efa4e0d82f
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-06 16:32:56 +1000

    Updating Search function.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,24 +109,6 @@
 
     //test printing of matchDetails
     //print_r($matches);
-
-        echo "
-        <html>
-            input[type=text] {
-            width: 130px;
-            box-sizing: border-box;
-            border: 2px solid #ccc;
-            border-radius: 4px;
-            font-size: 16px;
-            background-color: white;
-            background-image: url('searchicon.png');
-            background-position: 10px 10px;
-            background-repeat: no-repeat;
-            padding: 12px 20px 12px 40px;
-            -webkit-transition: width 0.4s ease-in-out;
-            transition: width 0.4s ease-in-out;
-        }
-        </html>"
 ?>
 
 @section('content')
@@ -146,6 +128,24 @@
         </div>
     </div>
     @endif
+
+    <div>
+        input[type=text] {
+        width: 130px;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        font-size: 16px;
+        background-color: white;
+        background-image: url('searchicon.png');
+        background-position: 10px 10px;
+        background-repeat: no-repeat;
+        padding: 12px 20px 12px 40px;
+        -webkit-transition: width 0.4s ease-in-out;
+        transition: width 0.4s ease-in-out;
+        }
+    </div>
+
 @endfor
     <!-- Need w3.js to use their methods -->
     <script src="https://www.w3schools.com/lib/w3.js"></script>

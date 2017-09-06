e9b360b63b83fa6840f7e7636589d803effaec0d
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-09-06 21:43:31 +1000

    Added updateFilter() js func in home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,4 +149,9 @@
     <!-- Need w3.js to use their methods -->
     <script src="https://www.w3schools.com/lib/w3.js"></script>
     <script src="{{ asset('js/home.js') }}"></script>
+    <script>
+    function updateFilter(val){
+        document.getElementById("filterPercent").innerHTML=val+"%";
+    }
+   </script>  
 @endsection

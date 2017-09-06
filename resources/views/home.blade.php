8ac476666793da06eab24a16818efc14bc637062
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-06 16:27:58 +1000

    Updating Search function.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,6 @@
 @extends('layouts.app')
 <link href="{{ asset('css/home.css') }}" rel="stylesheet">
 
-<html>
-    <body>
-        <style>
-        input[type=text] {
-            width: 130px;
-            box-sizing: border-box;
-            border: 2px solid #ccc;
-            border-radius: 4px;
-            font-size: 16px;
-            background-color: white;
-            background-image: url('searchicon.png');
-            background-position: 10px 10px;
-            background-repeat: no-repeat;
-            padding: 12px 20px 12px 40px;
-            -webkit-transition: width 0.4s ease-in-out;
-            transition: width 0.4s ease-in-out;
-        }
-        </style>
-    </body>
-</html>
 
 
 
@@ -129,6 +109,22 @@
 
     //test printing of matchDetails
     //print_r($matches);
+
+        echo "
+            input[type=text] {
+            width: 130px;
+            box-sizing: border-box;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            background-color: white;
+            background-image: url('searchicon.png');
+            background-position: 10px 10px;
+            background-repeat: no-repeat;
+            padding: 12px 20px 12px 40px;
+            -webkit-transition: width 0.4s ease-in-out;
+            transition: width 0.4s ease-in-out;
+        }"
 ?>
 
 @section('content')

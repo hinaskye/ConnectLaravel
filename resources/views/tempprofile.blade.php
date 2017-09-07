d6215ccc7ed3fa9ac8ed14c842b8e299f278d0ed
Michael <s3539352@student.rmit.edu.au>
2017-09-07 11:17:29 +1000

    Updated Code for DOB to age on tempprofile.blade.php

--- resources/views/tempprofile.blade.php
+++ resources/views/tempprofile.blade.php
@@ -42,7 +42,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
           <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-grey"></i>Melbourne, AU (Hard-Coded)</p>
           <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-grey"></i><?php echo $user->email; ?></p>
           <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-grey"></i>1800-333-000</p>
-          <p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-large w3-text-grey"></i><?php echo $user->birthday; ?></p>
+          <p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-large w3-text-grey"></i><?php
+            $from = new DateTime($user->birthday);
+            $to = new DateTime('today');
+            echo $from->diff($to)->y, " years old";?></p>
           <hr>
           <br>
         </div>

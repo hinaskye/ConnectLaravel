552d4330884ed0f3409ce39150fcf5c4520c8311
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-10 15:49:11 +1000

    Updating issue

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -220,6 +220,10 @@
                             </div>
                         </div>
 
+
+
+
+
                     <div class="form-group">
                         <div class="col-lg-10 col-lg-offset-2">
                             <button type="reset" class="btn btn-default">Cancel</button>

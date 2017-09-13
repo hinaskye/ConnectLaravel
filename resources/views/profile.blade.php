1fee3b0c4d6287efcf5a8b99cb3ad0bf4a898f11
Chan Hoe Teng (Philip) <s3536311@student.rmit.edu.au>
2017-09-13 23:12:04 +1000

    update animation

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -29,7 +29,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
     <!-- Left Column -->
     <div class="w3-third">
 
-      <div class="w3-white w3-text-grey w3-card-4 .slide-image">
+      <div class="w3-white w3-text-grey w3-card-4 slide-image">
         <div class="w3-display-container">
           <img src="https://i.ytimg.com/vi/2qZHh_iN5Zs/hqdefault.jpg" style="width:100%" alt="Avatar">
 

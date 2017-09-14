68d874538ec2e68919390778eb6987f7b4b41d3f
Hieu Ngo <s3502260@student.rmit.edu.au>
2017-09-14 16:06:08 +1000

    changed url of settings button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,7 @@
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                     <br>
-                    <a href="#"><button type="button" class="btn btn-success">Settings</button></a>
+                    <a href="{{ url('/settings') }}"><button type="button" class="btn btn-success">Settings</button></a>
                 </div>
             </div>
 

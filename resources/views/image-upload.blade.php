6985892d4c78e920bf05b244b8c51d6d0b7335b3
s3557584 <s3557584@student.rmit.edu.au>
2017-09-15 02:35:32 +1000

    Push to test

--- resources/views/image-upload.blade.php
+++ resources/views/image-upload.blade.php
@@ -7,7 +7,7 @@
 <body>
 
 <div class="container">
-	{!!Html::image(getImage($myPath),'logo',['width'=>60,'height'=>55])!!}
+	{{ Html::image($myPath) }}
   <div class="panel panel-primary">
     <div class="panel-heading"><h2>Laravel 5.3 Amazon S3 Image Upload with Validation example</h2></div>
 

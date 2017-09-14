1f248127a1f337ed777f8f758e8cd2ff258ff4e0
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-09-14 22:32:35 +1000

    added bootstrap link

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,6 +12,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

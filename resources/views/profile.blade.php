322797be4707528be71d9eead1ad9cf91d2e5a39
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-09-15 00:16:43 +1000

    revert css link

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,6 +12,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
 <body>
 

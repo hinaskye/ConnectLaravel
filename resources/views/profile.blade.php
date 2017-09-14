a10e684cda39567645a0bb3a9c03c5304c103115
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-09-14 22:29:00 +1000

    update bs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -24,10 +24,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
 <div class="container" style="max-width:1400px;">
 
   <!-- The Grid -->
-  <div class="row p-10">
+  <div class="row p-4">
 
     <!-- Left Column -->
-    <div class="w3-third">
+    <div class="float-sm-left">
 
       <div class="w3-white w3-text-grey w3-card-4">
         <div class="col-lg-20">

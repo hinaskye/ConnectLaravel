e33b1ca7269adbb7102c387a65db15306eec395b
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-09-15 01:38:36 +1000

    add animation

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,7 +22,7 @@
   <div class="row">
 
     <!-- Left Column -->
-    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 animate-left">
 
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 background-gray">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-top-10">

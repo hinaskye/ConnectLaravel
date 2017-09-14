02886b280258cd4478186ca143a441639a43c121
s3557584 <s3557584@student.rmit.edu.au>
2017-09-15 09:45:17 +1000

    push to test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile', function () {
+Route::get('/profile', 'S3ImageController@imageUploadPost', function () {
     return view('profile');
 });
 

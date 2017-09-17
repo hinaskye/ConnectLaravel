a9593781a1597c7eb5199b5bc4a4ca9de6b8a0c5
s3557584 <s3557584@student.rmit.edu.au>
2017-09-18 01:21:08 +1000

    push to test

--- app/Http/Controllers/S3ImageController.php
+++ app/Http/Controllers/S3ImageController.php
@@ -31,11 +31,11 @@ class S3ImageController extends Controller
         $this->validate($request, [
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-
-        $imageName = time().'.'.$request->image->getClientOriginalExtension();
+        $imageName = $userID;
+       // $imageName = time().'.'.$request->image->getClientOriginalExtension();
         $image = $request->file('image');
         $t = Storage::disk('s3')->put($imageName, file_get_contents($image), 'public');
-        $imageName = $userID;
+        $imageName = Storage::disk('s3')->url($imageName);
         return back()
             ->with('success','Image Uploaded successfully.')
             ->with('path',$imageName);

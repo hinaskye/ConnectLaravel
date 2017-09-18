4b89e7255b981fb4b7e02ab47c4fc1f1148255b1
s3557584 <s3557584@student.rmit.edu.au>
2017-09-18 13:04:21 +1000

    push to test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,7 +149,6 @@
         $key = $matches[$matchCount]['user']['id'];
 
         $file['url']= 'https://s3-ap-southeast-2.amazonaws.com/profile.pictures.pp'.'/'.$key;
-        echo $key;
         echo $file['url'];
     ?>
     <div class="card col-md-4 col-sm-6">

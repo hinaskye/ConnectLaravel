d4fcd821aae6a63bfa41c7a5b11b889e93ce14f1
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-20 22:14:47 +1000

    trying to get array_push to work with counter, getting NULL error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -176,8 +176,8 @@
             $kilometres = $miles * 1.6;
             var_dump($kilometres);
             var_dump($miles);
-            array_push($matches[$counter], $pcRow['suburb']);
-            array_push($matches[$counter], $kilometres);
+            //array_push($matches[$counter], $pcRow['suburb']);
+            //array_push($matches[$counter], $kilometres);
             ++$counter;
             var_dump($pcRow);
             var_dump($counter);

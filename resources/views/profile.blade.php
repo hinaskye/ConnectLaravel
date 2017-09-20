2f94560ae2511ccc3f00be786809b1d00c230de9
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-09-20 23:32:49 +1000

    bug fixes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -31,8 +31,10 @@
         </div>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <br>
-          <legend class="text-white pull-left"><?php echo $user->firstname," ", $user->lastname; ?></legend>
-          <button onclick="toggleSetting()"><i class="fa fa-cog fa-fw margin-right-16 text-large text-grey pull-right"></i></button>
+          <legend class="text-white">
+          	<?php echo $user->firstname," ", $user->lastname; ?>
+          	<button onclick="toggleSetting()"><i class="fa fa-cog fa-fw margin-right-16 text-large text-grey pull-right"></i></button>
+          </legend>
           <p><i class="fa fa-user fa-fw margin-right-16 text-large text-grey"></i><?php echo $user->gender; ?></p>
           <p><i class="fa fa-envelope fa-fw margin-right-16 text-large text-grey"></i><?php echo $user->email; ?></p>
           <p><i class="fa fa-birthday-cake fa-fw margin-right-16 text-large text-grey"></i><?php

9fca0a1cafa3ed2367460b82bb13ee3e472e10df
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-22 09:13:18 +1000

    Updating user card to show distance and suburb.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -157,6 +157,7 @@
             $currentUserLonConvert = floatval($currentUserLon);
 
 
+            //Longitude & Latitude to distance algorithm (code based of code from: http://www.geodatasource.com/developers/php)
             $unit = "K";
 
             $theta = $currentUserLonConvert - $logUserLonCallConvert;
@@ -225,6 +226,8 @@
         <div class="card-body">
             <h3 class="card-title">{{$matches[$matchCount]['user']['firstname']}} {{$matches[$matchCount]['user']['lastname']}}</h3>
             <p class="card-text">{{$matches[$matchCount]['user']['birthday']}}</p>
+            <p class="card-text">{{$matches[$matchCount]['locoArray']['distance']}}</p>
+            <p class="card-text">{{$matches[$matchCount]['locoArray']['suburb']}}</p>
             <p class="card-text">{{$matches[$matchCount]['user']['postcode']}}</p>
         </div>
     </div>

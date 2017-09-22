352b6f6b1fd684cdf100675ec99d800c3c2cf091
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-22 17:28:57 +1000

    updating user DB with new columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -39,7 +39,7 @@ class CreateUsersTable extends Migration
             $table->integer('q10');
             $table->string('aboutme');
             $table->integer('postcode');
-            $table->ineger('favourite');
+            $table->integer('favourite');
         });
     }
 

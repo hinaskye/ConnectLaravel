c8b8125378972ddc1e03d40ba267e1b0d41dda4d
rhysellwood <s3491191@student.rmit.edu.au>
2017-09-22 10:01:35 +1000

    rounding Kilometres so it isn't a huge number

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -228,7 +228,7 @@
             <h3 class="card-title">{{$matches[$matchCount]['user']['firstname']}} {{$matches[$matchCount]['user']['lastname']}}</h3>
             <p class="card-text">{{$matches[$matchCount]['user']['birthday']}}</p>
             <p class="card-text">{{$matches[$matchCount]['user']['postcode']}}</p>
-            <p class="card-text">{{$matches[$matchCount]['0']['distance']}}</p>
+            <p class="card-text">{{"~".$matches[$matchCount]['0']['distance']."KMs"}}</p>
             <p class="card-text">{{$matches[$matchCount]['0']['suburb']}}</p>
         </div>
     </div>

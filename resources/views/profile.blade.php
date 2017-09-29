84b42c90065f77c7c39104fde675df1ce8d18b9b
s3557584 <s3557584@student.rmit.edu.au>
2017-09-29 14:38:41 +1000

    Displaying uploaded image at profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,7 +12,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--this css links gives the logo-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+<img class="card-img-top" src="{!! $file['url'] !!}" width="100%" alt="Image">
 <body>
 
 <!-- Page Container -->

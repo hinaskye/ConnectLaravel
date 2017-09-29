2efc644e3a4aee9d9aeb39fdaa76422cd139f161
s3557584 <s3557584@student.rmit.edu.au>
2017-09-29 14:30:37 +1000

    Moving code to the correct profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -27,6 +27,46 @@
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 background-gray">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-top-10">
           <img src="{{ asset('images/profile.jpg') }}" style="width:100%" alt="Avatar">
+          <div class="container">
+  <div class="panel panel-primary">
+    <div class="panel-heading"><h2>Laravel 5.3 Amazon S3 Image Upload with Validation example</h2></div>
+
+    <div class="panel-body">
+
+      @if (count($errors) > 0)
+   <div class="alert alert-danger">
+      <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+     </ul>
+      </div>
+      @endif
+
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block">
+      <button type="button" class="close" data-dismiss="alert">×</button>
+            <strong>{{ $message }}</strong>
+    </div>
+    <img src="1504198936.jpg">
+    @endif
+
+    <form action="{{ url('s3-image-upload') }}" enctype="multipart/form-data" method="POST">
+    {{ csrf_field() }}
+    <div class="row">
+      <div class="col-md-12">
+        <input type="file" name="image" />
+      </div>
+      <div class="col-md-12">
+        <button type="submit" class="btn btn-success">Upload</button>
+      </div>
+    </div>
+    </form>
+    </div>
+
+  </div>
+</div>
 
         </div>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
--- resources/views/uniqueprofile.blade.php
+++ resources/views/uniqueprofile.blade.php
@@ -12,45 +12,6 @@
 
 <!-- Page Container -->
 <div class="container" style="max-width:1400px;">
-  <div class="container">
-  <div class="panel panel-primary">
-    <div class="panel-heading"><h2>Laravel 5.3 Amazon S3 Image Upload with Validation example</h2></div>
-
-    <div class="panel-body">
-
-      @if (count($errors) > 0)
-   <div class="alert alert-danger">
-      <strong>Whoops!</strong> There were some problems with your input.<br><br>
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-     </ul>
-      </div>
-      @endif
-
-    @if ($message = Session::get('success'))
-    <div class="alert alert-success alert-block">
-      <button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>{{ $message }}</strong>
-    </div>
-    @endif
-
-    <form action="{{ url('profile') }}" enctype="multipart/form-data" method="POST">
-    {{ csrf_field() }}
-    <div class="row">
-      <div class="col-md-12">
-        <input type="file" name="image" />
-      </div>
-      <div class="col-md-12">
-        <button type="submit" class="btn btn-success">Upload</button>
-      </div>
-    </div>
-    </form>
-    </div>
-
-  </div>
-</div>
 
   <!-- The Grid -->
   <div class="row">

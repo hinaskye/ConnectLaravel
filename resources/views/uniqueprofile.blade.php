fa6bd5b30db1e467f7d5f63c0bcb1c0e7568258b
s3557584 <s3557584@student.rmit.edu.au>
2017-09-29 14:24:12 +1000

    Upload image function to profile page

--- resources/views/uniqueprofile.blade.php
+++ resources/views/uniqueprofile.blade.php
@@ -22,6 +22,34 @@
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 background-gray">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-top-10">
           <img src="{{ asset('images/profile.jpg') }}" style="width:100%" alt="Avatar">
+           @if (count($errors) > 0)
+            <div class="alert alert-danger">
+               <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+             @foreach ($errors->all() as $error)
+               <li>{{ $error }}</li>
+             @endforeach
+            </ul>
+          </div>
+           @endif
+
+          @if ($message = Session::get('success'))
+          <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+                  <strong>{{ $message }}</strong>
+    </div>
+    @endif
+          <form action="{{ url('profile') }}" enctype="multipart/form-data" method="POST">
+            {{ csrf_field() }}
+           <div class="row">
+              <div class="col-md-12">
+                <input type="file" name="image" />
+              </div>
+              <div class="col-md-12">
+                <button type="submit" class="btn btn-success">Upload</button>
+              </div>
+            </div>
+          </form>
 
         </div>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">

4e4c76c512d990d8c1496c336c946867400beb19
Michael <s3539352@student.rmit.edu.au>
2017-09-29 13:44:40 +1000

    Minor form change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -272,8 +272,8 @@ $conn->close();
         </div><br>
         @for($matchCount=0; $matchCount<count($matches); $matchCount++)
             @if($matches[$matchCount]['matchPcent'] != 0)
-            <form id="idForm" method="POST" action="/uniqueprofile">
                 <div class="card col-md-4 col-sm-6">
+                  <form id="idForm" method="POST" action="/uniqueprofile">
                     <p class="matchingPercent">{{$matches[$matchCount]['matchPcent']}}%</p>
                     <img class="card-img-top" src="/images/blank-female-profile-user.png" width="100%" alt="Match Image">
                     <div class="card-body">

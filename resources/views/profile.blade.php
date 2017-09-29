6bd73a2b9127686ec7f5e2b41280d3574dcfe3bf
s3557584 <s3557584@student.rmit.edu.au>
2017-09-29 14:46:00 +1000

    Fixing Error

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,11 +6,10 @@
   $userID = $user->id;
 ?>
 <?php
-                $key = $userID;
+                $key = $matches[$matchCount]['user']['id'];
 
                 $file['url']= 'https://s3-ap-southeast-2.amazonaws.com/profile.pictures.pp'.'/'.$key;
             ?>
-
 @section('content')
 <html>
 <title>Connect Profile</title>
@@ -18,6 +17,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--this css links gives the logo-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="{{ asset('css/profile.css') }}" rel="stylesheet">
 <body>
 
 <!-- Page Container -->

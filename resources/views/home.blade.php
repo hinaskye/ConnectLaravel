b9611eac668a49efa6a6330ed4f9ab474072f307
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-06 12:04:53 +1100

    Updating algorithm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,214 +3,236 @@
 <link href="{{ asset('css/home.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<?php
-//matching Algorithm code.
-//creating connection to database with details
-$servername = "connectdb.ckktlmrdu53g.ap-southeast-2.rds.amazonaws.com";
-$username = "ConnectAdmin";
-$password = "password";
-$dbname = "UserDB";
+    <?php
+    //matching Algorithm code.
+    //creating connection to database with details
+    $servername = "connectdb.ckktlmrdu53g.ap-southeast-2.rds.amazonaws.com";
+    $username = "ConnectAdmin";
+    $password = "password";
+    $dbname = "UserDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if($conn->connect_error){
-    die("Connection Failed: " . $conn->connect_error);
-}
-//test to make sure DB connection is working.
-$sql = "SELECT id FROM users";
-$result = $conn->query($sql);
+    if($conn->connect_error){
+        die("Connection Failed: " . $conn->connect_error);
+    }
+    //test to make sure DB connection is working.
+    $sql = "SELECT id FROM users";
+    $result = $conn->query($sql);
 
 
-//ID of the current user logged in
-$user = Auth::user();
-$userID = $user->id;
+    //ID of the current user logged in
+    $user = Auth::user();
+    $userID = $user->id;
 
-//Getting the maximum ID of all the users in the DB
-$maxSql = "SELECT max(id) as id FROM users";
-$mResult=mysqli_query($conn,$maxSql);
-$row = mysqli_fetch_assoc($mResult);
-$maxID = $row['id'];
+    //Getting the maximum ID of all the users in the DB
+    $maxSql = "SELECT max(id) as id FROM users";
+    $mResult=mysqli_query($conn,$maxSql);
+    $row = mysqli_fetch_assoc($mResult);
+    $maxID = $row['id'];
 
-$i = 1; //used as a counter int
-$currentID = 1; //current ID of User in DB
-$matchPcent = 0; //match percentage for users
+    $i = 1; //used as a counter int
+    $currentID = 1; //current ID of User in DB
+    $matchPcent = 0; //match percentage for users
 
-$matches = array(); //Keep track of all matches and their percentage
+    $matches = array(); //Keep track of all matches and their percentage
 
-//check to make sure have not reached the end of the DB
-while ($i <= $maxID){
+    //check to make sure have not reached the end of the DB
+    while ($i <= $maxID){
 
-    //Check to make sure you are not matching yourself
-    if ($userID != $currentID){
+        //Check to make sure you are not matching yourself
+        if ($userID != $currentID){
 
-        //Query DB for First users in DB answers to questions
-        $questionSql = "SELECT firstname, lastname, gender,looking, myedu, matchingedu, birthday,
+            //Query DB for First users in DB answers to questions
+            $questionSql = "SELECT firstname, lastname, gender,looking, myedu, matchingedu, birthday,
             q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, id, aboutme, postcode, favourite
             FROM users WHERE id = $currentID";
-        $questionResult = mysqli_query($conn, $questionSql);
-        $row = mysqli_fetch_assoc($questionResult);
-        $matchPcent = 0;
-        if ($user->looking == $row['gender'] or $user->looking == "both"){
+            $questionResult = mysqli_query($conn, $questionSql);
+            $row = mysqli_fetch_assoc($questionResult);
+            $matchPcent = 0;
+            if ($user->looking == $row['gender'] or $user->looking == "both"){
 
-            if($user->matchingedu == $row['myedu']){
-                if ($user->q1 == $row ['q1']){
-                    $matchPcent += 10;
-                }
-                if ($user->q2 == $row ['q2']){
-                    $matchPcent += 10;
-                }
-                if ($user->q3 == $row ['q3']){
-                    $matchPcent += 10;
-                }
-                if ($user->q4 == $row ['q4']){
-                    $matchPcent += 10;
-                }
-                if ($user->q5 == $row ['q5']){
-                    $matchPcent += 10;
-                }
-                if ($user->q6 == $row ['q6']){
-                    $matchPcent += 10;
-                }
-                if ($user->q7 == $row ['q7']){
-                    $matchPcent += 10;
-                }
-                if ($user->q8 == $row ['q8']){
-                    $matchPcent += 10;
-                }
-                if ($user->q9 == $row ['q9']){
-                    $matchPcent += 10;
-                }
-                if ($user->q10 == $row ['q10']){
-                    $matchPcent += 10;
+                if($user->matchingedu == $row['myedu']){
+                    if ($user->q1 == $row ['q1']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q2 == $row ['q2']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q3 == $row ['q3']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q4 == $row ['q4']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q5 == $row ['q5']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q6 == $row ['q6']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q7 == $row ['q7']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q8 == $row ['q8']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q9 == $row ['q9']){
+                        $matchPcent += 10;
+                    }
+                    if ($user->q10 == $row ['q10']){
+                        $matchPcent += 10;
+                    }
+
+                    $matchDetails = array("user"=>$row, "matchPcent"=>$matchPcent); //may want to change $row to $match
+                    array_push($matches, $matchDetails);
+
                 }
 
-                $matchDetails = array("user"=>$row, "matchPcent"=>$matchPcent); //may want to change $row to $match
-                array_push($matches, $matchDetails);
             }
 
         }
-
+        $currentID ++;
+        $i++;
     }
-    $currentID ++;
-    $i++;
-}
 
 
 
-//postcode to suburb algorithm
+    //postcode to suburb algorithm
 
-$counter = 0;
+    $counter = 0;
 
-//query user DB to get the postcodes of all users are put them in an array
-$userPostCodeSQL = "SELECT postcode FROM users";
-$userPostCodeResult=mysqli_query($conn,$userPostCodeSQL);
-$userPostCode = mysqli_fetch_assoc($userPostCodeResult);
+    //query user DB to get the postcodes of all users are put them in an array
+    $userPostCodeSQL = "SELECT postcode FROM users";
+    $userPostCodeResult=mysqli_query($conn,$userPostCodeSQL);
+    $userPostCode = mysqli_fetch_assoc($userPostCodeResult);
 
-$userPostCodeQuery = mysqli_query($conn, $userPostCodeSQL);
-$userPostcodeArray = array();
-
-
-$lowestSQL = "SELECT min(id) as id FROM users";
-$lResult=mysqli_query($conn,$lowestSQL);
-$row = mysqli_fetch_assoc($lResult);
-$lowID = $row['id'];
-
-$counter = 0;
-$loopingID = (int)$lowID;
-
-//loops through every postcode in the user table and returns each postcode
-while ($loopRow = mysqli_fetch_assoc($userPostCodeQuery)){
-
-    if ($userID != $loopingID){
-
-        if ($counter < sizeof($matches)){
-
-            if ($matches[$counter]['user']['id'] >= 0 ){
-
-                $userPostcodeArray[] = $loopRow;
-                $searchPC = $loopRow['postcode'];
-
-                //gives all the data for a row that matches the postcode of the users
-                $postcodeSql = "SELECT id, postcode, suburb, state, latitude, longitude
-                    FROM postcodes WHERE postcode = $searchPC";
-                $pcResult = mysqli_query($conn, $postcodeSql);
-                $pcRow = mysqli_fetch_assoc($pcResult);
-
-                //query user DB to get the postcodes of all users are put them in an array
-                $userPostCodeSQL = "SELECT postcode FROM users";
-                $userPostCodeResult=mysqli_query($conn,$userPostCodeSQL);
-                $userPostCode = mysqli_fetch_assoc($userPostCodeResult);
-
-                //distance from user algorithm
-
-                //get the users postcode
-                $logUserPostCodeSQL = "SELECT postcode FROM users WHERE id = $userID";
-                $logUserPostCodeResult = mysqli_query($conn, $logUserPostCodeSQL);
-                $logUserPostCode = mysqli_fetch_assoc($logUserPostCodeResult);
-                $logUserPostCodeCall = $logUserPostCode['postcode'];
-
-                $lowestSQL = "SELECT min(id) as id FROM users";
-                $lResult=mysqli_query($conn,$lowestSQL);
-                $row = mysqli_fetch_assoc($lResult);
-                $lowID = $row['id'];
-
-                //get the users postcode to get latitude and longitude
-                $logUserLatSQL = "SELECT latitude FROM postcodes WHERE postcode = $logUserPostCodeCall";
-                $logUserLatResult = mysqli_query($conn, $logUserLatSQL);
-                $logUserLat = mysqli_fetch_assoc($logUserLatResult);
-                $logUserLatCall = $logUserLat['latitude'];
-
-                $logUserLonSQL = "SELECT longitude FROM postcodes WHERE postcode = $logUserPostCodeCall";
-                $logUserLonResult = mysqli_query($conn, $logUserLonSQL);
-                $logUserLon = mysqli_fetch_assoc($logUserLonResult);
-                $logUserLonCall = $logUserLon['longitude'];
+    $userPostCodeQuery = mysqli_query($conn, $userPostCodeSQL);
+    $userPostcodeArray = array();
 
 
-                $logUserLatCallConvert = floatval($logUserLatCall);
-                $logUserLonCallConvert = floatval($logUserLonCall);
-
-                $currentUserLat = $pcRow['latitude'];
-                $currentUserLatConvert = floatval($currentUserLat);
-
-                $currentUserLon = $pcRow['longitude'];
-                $currentUserLonConvert = floatval($currentUserLon);
+    $lowestSQL = "SELECT min(id) as id FROM users";
+    $lResult=mysqli_query($conn,$lowestSQL);
+    $row = mysqli_fetch_assoc($lResult);
+    $lowID = $row['id'];
 
 
-                //Longitude & Latitude to distance algorithm (code based of code from: http://www.geodatasource.com/developers/php)
-                $unit = "K";
 
-                $theta = $currentUserLonConvert - $logUserLonCallConvert;
-                $dist = sin(deg2rad($currentUserLatConvert)) * sin(deg2rad($logUserLatCallConvert)) +  cos(deg2rad($currentUserLatConvert)) * cos(deg2rad($logUserLatCallConvert)) * cos(deg2rad($theta));
-                $dist = acos($dist);
-                $dist = rad2deg($dist);
-                $miles = $dist * 60 * 1.1515;
-                $unit = strtoupper($unit);
-                //converting to Kilometres
-                $kilometresFloat = $miles * 1.6;
-                $kilometres = round($kilometresFloat);
-                $locoArray = array("suburb"=>$pcRow['suburb'], "distance"=>$kilometres);
+    $loopingID = (int)$lowID;
 
-                array_push($matches[$counter], $locoArray);
-                ++$counter;
+    //loops through every postcode in the user table and returns each postcode
 
+    while ($loopRow = mysqli_fetch_assoc($userPostCodeQuery)){
+
+        if ($userID != $loopingID){
+
+            if ($counter < sizeof($matches)){
+
+                if ($matches[$counter]['user']['id'] >= 0 ){
+
+                    $correctPostCode = $matches[$counter]['user']['postcode'];
+
+                    $userPostCodeSQL = "SELECT postcode FROM users WHERE postcode = $correctPostCode";
+                    $userPostCodeResult = mysqli_query($conn,$userPostCodeSQL);
+                    $userPostCode = mysqli_fetch_assoc($userPostCodeResult);
+                    $userPostCodeQueryLoopRow = mysqli_query($conn, $userPostCodeSQL);
+
+                    $postCodeLoopRow = mysqli_fetch_assoc($userPostCodeQueryLoopRow);
+
+
+                    var_dump($correctPostCode);
+
+                    $userPostcodeArray[] = $postCodeLoopRow;
+                    $searchPC = $postCodeLoopRow['postcode'];
+
+                    //gives all the data for a row that matches the postcode of the users
+                    $postcodeSql = "SELECT id, postcode, suburb, state, latitude, longitude
+                     FROM postcodes WHERE postcode = $searchPC";
+                    $pcResult = mysqli_query($conn, $postcodeSql);
+                    $pcRow = mysqli_fetch_assoc($pcResult);
+
+                    var_dump($pcRow);
+
+                    //query user DB to get the postcodes of all users are put them in an array
+                    $userPostCodeSQL = "SELECT postcode FROM users";
+                    $userPostCodeResult=mysqli_query($conn,$userPostCodeSQL);
+                    $userPostCode = mysqli_fetch_assoc($userPostCodeResult);
+
+                    //distance from user algorithm
+
+                    //get the users postcode
+                    $logUserPostCodeSQL = "SELECT postcode FROM users WHERE id = $userID";
+                    $logUserPostCodeResult = mysqli_query($conn, $logUserPostCodeSQL);
+                    $logUserPostCode = mysqli_fetch_assoc($logUserPostCodeResult);
+                    $logUserPostCodeCall = $logUserPostCode['postcode'];
+
+                    $lowestSQL = "SELECT min(id) as id FROM users";
+                    $lResult=mysqli_query($conn,$lowestSQL);
+                    $row = mysqli_fetch_assoc($lResult);
+                    $lowID = $row['id'];
+
+                    //get the users postcode to get latitude and longitude
+                    $logUserLatSQL = "SELECT latitude FROM postcodes WHERE postcode = $logUserPostCodeCall";
+                    $logUserLatResult = mysqli_query($conn, $logUserLatSQL);
+                    $logUserLat = mysqli_fetch_assoc($logUserLatResult);
+                    $logUserLatCall = $logUserLat['latitude'];
+
+                    $logUserLonSQL = "SELECT longitude FROM postcodes WHERE postcode = $logUserPostCodeCall";
+                    $logUserLonResult = mysqli_query($conn, $logUserLonSQL);
+                    $logUserLon = mysqli_fetch_assoc($logUserLonResult);
+                    $logUserLonCall = $logUserLon['longitude'];
+
+
+                    $logUserLatCallConvert = floatval($logUserLatCall);
+                    $logUserLonCallConvert = floatval($logUserLonCall);
+
+                    $currentUserLat = $pcRow['latitude'];
+                    $currentUserLatConvert = floatval($currentUserLat);
+
+                    $currentUserLon = $pcRow['longitude'];
+                    $currentUserLonConvert = floatval($currentUserLon);
+
+
+                    //Longitude & Latitude to distance algorithm (code based of code from: http://www.geodatasource.com/developers/php)
+                    $unit = "K";
+
+                    $theta = $currentUserLonConvert - $logUserLonCallConvert;
+                    $dist = sin(deg2rad($currentUserLatConvert)) * sin(deg2rad($logUserLatCallConvert)) +  cos(deg2rad($currentUserLatConvert)) * cos(deg2rad($logUserLatCallConvert)) * cos(deg2rad($theta));
+                    $dist = acos($dist);
+                    $dist = rad2deg($dist);
+                    $miles = $dist * 60 * 1.1515;
+                    $unit = strtoupper($unit);
+
+                    //converting to Kilometres
+                    $kilometresFloat = $miles * 1.6;
+                    $kilometres = round($kilometresFloat);
+                    $locoArray = array("suburb"=>$pcRow['suburb'], "distance"=>$kilometres);
+
+                    array_push($matches[$counter], $locoArray);
+                    //var_dump($pcRow,$loopRow,$userPostcodeArray);
+                    ++$counter;
+
+
+                }
             }
+
         }
-
+        $loopingID++;
     }
-    $loopingID++;
-}
+    var_dump($matches);
 
 
-//sort matches by highest match percentage, uses anonymous function
-usort($matches, function($a, $b){
-    return $b['matchPcent'] - $a['matchPcent'];
-});
+    //sort matches by highest match percentage, uses anonymous function
+    usort($matches, function($a, $b){
+        return $b['matchPcent'] - $a['matchPcent'];
+    });
 
-$conn->close();
+    $conn->close();
 
-//test printing of matchDetails
-//print_r($matches);
-?>
+    //test printing of matchDetails
+    //print_r($matches);
+    ?>
+
 
 <?php
         $user = Auth::user();

0849bce38475f37e4dbbd3933fea23c0cd464ee1
Chan Hoe Teng (Philip) <s3536311@student.rmit.edu.au>
2017-10-10 22:10:20 +1100

    test run the first test case

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -17,4 +17,37 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testRegisterNewUser(){
+    	$this->visit('/Register')
+    		 ->type('Test','firstname')
+    		 ->type('User','lastname')
+    		 ->type('Test User','username')
+    		 ->type('T.User@gmail.com','email')
+    		 ->type('123456','password')
+    		 ->type('123456','password_comfirmation')
+    		 ->type('1997-09-07','birthday')
+    		 ->select('gender','male')
+    		 ->select('myedu','Highschool')
+    		 ->select('matchingedu','Highschool')
+    		 ->select('q1','1')
+    		 ->select('q2','1')
+    		 ->select('q3','1')
+    		 ->select('q4','1')
+    		 ->select('q5','1')
+    		 ->select('q6','1')
+    		 ->select('q7','1')
+    		 ->select('q8','1')
+    		 ->select('q9','1')
+    		 ->select('q10','1')
+    		 ->type('Just a Tester','aboutme')
+    		 ->type('3000','postcode')
+    		 ->press('Register');
+    }
+    public function testLoginUser(){
+        $this->visit('/Login')
+             ->type('KP123@gmail.com','email')
+             ->type('123456','password')
+             ->press('Login');
+    }
 }

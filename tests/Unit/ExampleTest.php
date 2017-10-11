dbaa410bd2050c1acc10a66800117ccc170271de
Chan Hoe Teng (Philip) <s3536311@student.rmit.edu.au>
2017-10-11 23:41:40 +1100

    extend new class

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,11 +3,10 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ExampleTest extends \PHPUnit_Extensions_Selenium2TestCase
 {
     /**
      * A basic test example.

24562abbdaf6ed1609211d8f969f2cc39313c654
Chan Hoe Teng (Philip) <s3536311@student.rmit.edu.au>
2017-10-11 23:58:39 +1100

    change abstract class

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+abstract class ExampleTest extends TestCase
 {
     /**
      * A basic test example.

00206812b651eddfafaccbd1c03e814aa7597ae6
Hieu Ngo <s3502260@student.rmit.edu.au>
2017-10-12 22:17:09 +1100

    Reverted example unit test to fresh version

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,52 +3,18 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Laracasts\Integrated\Extensions\Laravel as IntegrationTest;
 
-class ExampleTest extends IntegrationTest
+class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
-        $this->visit('/Login')
-             ->type('KP123@gmail.com','email')
-             ->type('123456','password')
-             ->press('Login')
-             ->seePageIs('/home')
-             ->see('Katy Perry');
+        $this->assertTrue(true);
     }
 }
-    // public function testRegisterNewUser(){
-    // 	$this->visit('/Register')
-    // 		 ->type('Test','firstname')
-    // 		 ->type('User','lastname')
-    // 		 ->type('Test User','username')
-    // 		 ->type('T.User@gmail.com','email')
-    // 		 ->type('123456','password')
-    // 		 ->type('123456','password_comfirmation')
-    // 		 ->type('1997-09-07','birthday')
-    // 		 ->select('gender','male')
-    // 		 ->select('myedu','Highschool')
-    // 		 ->select('matchingedu','Highschool')
-    // 		 ->select('q1','1')
-    // 		 ->select('q2','1')
-    // 		 ->select('q3','1')
-    // 		 ->select('q4','1')
-    // 		 ->select('q5','1')
-    // 		 ->select('q6','1')
-    // 		 ->select('q7','1')
-    // 		 ->select('q8','1')
-    // 		 ->select('q9','1')
-    // 		 ->select('q10','1')
-    // 		 ->type('Just a Tester','aboutme')
-    // 		 ->type('3000','postcode')
-    // 		 ->press('Register');
-    // }
-

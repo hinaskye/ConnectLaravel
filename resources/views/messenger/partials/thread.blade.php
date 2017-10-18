23c75b5978a4e50ed81478e91bb342d1b9160059
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-10-18 22:44:29 +1100

    testing CSS reference

--- resources/views/messenger/partials/thread.blade.php
+++ resources/views/messenger/partials/thread.blade.php
@@ -1,19 +1,21 @@
 <?php $class = $thread->isUnread(Auth::id()) ? 'alert-info' : ''; ?>
 
 
-<div class="row">
-    <div class="media alert {{ $class }}">
-        <h4 class="media-heading">
-            <a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a>
-            ({{ $thread->userUnreadMessagesCount(Auth::id()) }} unread)</h4>
-        <p>
-            {{ $thread->latestMessage->body }}
-        </p>
-        <p>
-            <small><strong>Creator:</strong> {{ $thread->creator()->name }}</small>
-        </p>
-        <p>
-            <small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small>
-        </p>
+<div class="container">
+    <div class="row">
+        <div class="media alert {{ $class }}">
+            <h4 class="media-heading">
+                <a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a>
+                ({{ $thread->userUnreadMessagesCount(Auth::id()) }} unread)</h4>
+            <p>
+                {{ $thread->latestMessage->body }}
+            </p>
+            <p>
+                <small><strong>Creator:</strong> {{ $thread->creator()->name }}</small>
+            </p>
+            <p>
+                <small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small>
+            </p>
+        </div>
     </div>
 </div>

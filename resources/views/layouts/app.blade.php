1c61e6ec1dc44185f9020a34974bb75fe67b7201
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-18 16:30:32 +1100

    Adding basic chat function to application

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,18 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top nav-background">
             <div class="container">
                 <div class="navbar-header">
+                    <!-- Left Side Of Navbar -->
+                    <div class="nav navbar-nav navbar-left nav-inline col-lg-3 col-md-3 col-sm-3 col-xs-4">
+                        <div class="nav-brand col-lg-12 col-mg-12 col-sm-12 col-xs-12"><a href="{{ url('/home') }}"><img src="{{ asset('images/Logo.png') }}" width =100%></a></div>
+                    </div>
                     <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
@@ -34,11 +40,7 @@
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-left">
-                        <li> <img src="{{ asset('images/Logo.png') }}" width =25%><li>
-                    </ul>
-
+                    
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
@@ -46,7 +48,7 @@
                             <li><a href="{{ route('auth.login') }}">Login</a></li>
                             <li><a href="{{ route('auth.register') }}">Register</a></li>
                         @else
-                        <div class="topwelcome">
+                        <div>
                             <h5 class="topusername">Welcome, <?php echo $user->username; ?></h5>
                             <li class="dropdown dropdownuser">
 
@@ -56,12 +58,10 @@
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                    <li>
+                                    <li class="dropdown-list">
                                       <a href="{{ url('/home') }}">Home</a>
                                       <a href="{{ url('/profile') }}">My Profile</a>
-                                      <a href="{{ url('/profile') }}">Setting</a>
                                       <a href="{{ url('/about') }}">Founders</a>
-                                      <a href="https://www.paypal.me/lcemocha">Donate Money Here</a>
                                       <a href="{{ route('auth.logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
 
                                         <form id="logout-form" action="{{ route('auth.logout') }}" method="POST" style="display: none;">
@@ -79,6 +79,40 @@
 
         @yield('content')
     </div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container-fluid" id="footer">
+            <div class="container">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <h3>Connect</h3>
+                    <ul>
+                        <li><a href="{{ url('/home') }}">Home</a></li>
+                        <li><a href="{{ url('/profile') }}">My Profile</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="{{ url('/about') }}">Founders</a></li>
+                        <li><a href="https://www.paypal.me">Donate Money</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <h3>Follow Us</h3>
+                    <!-- Linked do not link to real accounts, code from free site: https://bootsnipp.com/snippets/featured/social-icon-strip-footer -->
+                    <a href="https://www.facebook.com/connect"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+                    <a href="https://twitter.com/connect"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+                    <a href="https://plus.google.com/connect"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+                    <a href="mailto:connect@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+                </div>
+            </div>
+
+            <div class="container-fluid footer-bottom">
+                <br><p class="text-center"> Copyright © Connect 2017. All right reserved.</p>
+            </div>
+        </div>
+    </footer>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>

5cfd7565f42cdbace9790e9779be2f64b6c1567e
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-10-18 23:07:59 +1100

    change divs color

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -3,7 +3,5 @@
 @section('content')
     @include('messenger.partials.flash')
 
-    <div class= "background-div">
     @each('messenger.partials.thread', $threads, 'thread', 'messenger.partials.no-threads')
-	</div>
 @stop

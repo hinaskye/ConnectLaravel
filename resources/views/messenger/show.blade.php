84f167885ade8578497a932b4614e00fd9417106
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-10-18 22:20:30 +1100

    change thread.blade

--- resources/views/messenger/show.blade.php
+++ resources/views/messenger/show.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-lg-12 col-md-12">
-			<h1>My Messages</h1>
-		</div>
-	</div>
-	<div class="row">
-	    <div class="col-md-6">
-	        <h2>{{ $thread->subject }}</h2>
-	        @each('messenger.partials.messages', $thread->messages, 'message')
-	        @include('messenger.partials.form-message')
-	    </div>
-	</div>
-</div>
+    <div class="col-md-6">
+        <h1>{{ $thread->subject }}</h1>
+        @each('messenger.partials.messages', $thread->messages, 'message')
+
+        @include('messenger.partials.form-message')
+    </div>
 @stop

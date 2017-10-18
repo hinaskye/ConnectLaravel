b2f41f42a7315dba39f9162f353c0ed26ca2e8ea
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-10-18 22:21:46 +1100

    make changes to flash.blade

--- resources/views/messenger/partials/flash.blade.php
+++ resources/views/messenger/partials/flash.blade.php
@@ -1,3 +1,8 @@
+<div class="container">
+    <div class="row">
+        <h1>My Messages</h1>
+    </div>
+</div>
 @if (Session::has('error_message'))
     <div class="alert alert-danger" role="alert">
         {{ Session::get('error_message') }}
--- resources/views/messenger/partials/thread.blade.php
+++ resources/views/messenger/partials/thread.blade.php
@@ -1,23 +1,19 @@
 <?php $class = $thread->isUnread(Auth::id()) ? 'alert-info' : ''; ?>
 
-<div class="container">
-    <div class="row">
-        <h1>My Messages</h1>
-    </div>
-    <div class="row">
-        <div class="media alert {{ $class }}">
-            <h4 class="media-heading">
-                <a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a>
-                ({{ $thread->userUnreadMessagesCount(Auth::id()) }} unread)</h4>
-            <p>
-                {{ $thread->latestMessage->body }}
-            </p>
-            <p>
-                <small><strong>Creator:</strong> {{ $thread->creator()->name }}</small>
-            </p>
-            <p>
-                <small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small>
-            </p>
-        </div>
+
+<div class="row">
+    <div class="media alert {{ $class }}">
+        <h4 class="media-heading">
+            <a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a>
+            ({{ $thread->userUnreadMessagesCount(Auth::id()) }} unread)</h4>
+        <p>
+            {{ $thread->latestMessage->body }}
+        </p>
+        <p>
+            <small><strong>Creator:</strong> {{ $thread->creator()->name }}</small>
+        </p>
+        <p>
+            <small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small>
+        </p>
     </div>
 </div>

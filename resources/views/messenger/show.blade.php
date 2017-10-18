2b908471fb7572873743092c21bdc6b0c03e92c3
Chan Hoe Teng <s3536311@student.rmit.edu.au>
2017-10-18 22:07:28 +1100

    Test fix CSS on show.blade

--- resources/views/messenger/show.blade.php
+++ resources/views/messenger/show.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="col-md-6">
-        <h1>{{ $thread->subject }}</h1>
-        @each('messenger.partials.messages', $thread->messages, 'message')
-
-        @include('messenger.partials.form-message')
-    </div>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12">
+			<h1>My Messages</h1>
+		</div>
+	</div>
+	<div class="row">
+	    <div class="col-md-6">
+	        <h2>{{ $thread->subject }}</h2>
+	        @each('messenger.partials.messages', $thread->messages, 'message')
+	        @include('messenger.partials.form-message')
+	    </div>
+	</div>
+</div>
 @stop

39f393aaa3aa28bdbfeb04d4ba125c6caf2fc5d7
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-22 13:53:10 +1100

    captcha field now requires at least one

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use app\Traits\CaptchaTrait;
 
 class RegisterController extends Controller
 {

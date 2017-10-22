d7b914301933281b78acede75a4fff2a05c31e66
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-22 12:41:37 +1100

    captcha on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -238,4 +238,5 @@
 </div>
 <script src="http://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 @endsection

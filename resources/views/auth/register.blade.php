daf00928f71f4c1a0c0ac70e524db4105f307c81
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-22 13:04:45 +1100

    captcha on register update with env file

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -212,7 +212,7 @@
                                 pattern="^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$">
                         </div>
 
-
+                        <div class="g-recaptcha" data-sitekey="6LfvXDUUAAAAAITEune8Z_qw0FcfF9ffUGYT3WFM"></div>
                         <div class="g-recaptcha" data-sitekey="{{ env('RE_CAP_SITE') }}"></div>
 
 

47eeb5ca3caad634419cfa1638c6abaa48de795b
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-22 14:38:48 +1100

    Updating validator code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Validators\ReCaptcha;
 
 class AppServiceProvider extends ServiceProvider
 {

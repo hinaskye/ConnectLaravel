daa73faa7c7e0c602ca17e428c396894e122bf29
Hieu Ngo <s3502260@student.rmit.edu.au>
2017-10-24 17:37:43 +1100

    Displays correct image and alt of user, and prints their name on the message

--- resources/views/messenger/partials/messages.blade.php
+++ resources/views/messenger/partials/messages.blade.php
@@ -1,10 +1,10 @@
 <div class="media">
     <a class="pull-left" href="#">
-        <img src="//www.gravatar.com/avatar/{{ md5($message->user->email) }} ?s=64"
-             alt="{{ $message->user->name }}" class="img-circle">
+        <img src="https://s3-ap-southeast-2.amazonaws.com/profile.pictures.pp/{{ $message->user->id }}"
+             alt="{{ $message->user->firstname }}" class="img-circle" width="64" height="64">
     </a>
-    <div class="media-body">
-        <h5 class="media-heading">{{ $message->user->name }}</h5>
+    <div class="media-body background-div">
+        <h5 class="media-heading">{{ $message->user->firstname }}</h5>
         <p>{{ $message->body }}</p>
         <div class="text-muted">
             <small>Posted {{ $message->created_at->diffForHumans() }}</small>

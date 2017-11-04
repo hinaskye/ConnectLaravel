e048990409cf7bdd50ac0e587de8ba52ed5a47e4
s3557584 <s3557584@student.rmit.edu.au>
2017-11-04 18:24:37 +1100

    Fixing default image
    Added java script function for loading default image

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,6 +15,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
 <script src="{{ asset('js/validation.js') }}"></script>
+<script type="text/javascript">
+  function imgError(image) {
+    image.onerror = "";
+    image.src = "/images/blank-female-profile-user.png";
+    return true;
+}
+</script>
 <body>
 
   <!-- Page Container -->
